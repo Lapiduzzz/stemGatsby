25f216d8fde580cf601679fcbe9971d50b2e5b1a
Lapiduzzz <Lapiduz@ms>
2022-02-08 22:24:42 +0300

    add Context

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,4 +1,5 @@
-const React = require("react")
-const ContextProvider = require("./src/context/Context")
+import React from "react"
+import {ContextProvider} from "./src/context/Context";
 
 export const wrapRootElement = ({element}) => (<ContextProvider>{element}</ContextProvider>)
+
