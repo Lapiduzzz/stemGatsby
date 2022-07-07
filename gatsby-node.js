ba62ddbf8ff7d6944c098aa974345820def4f488
Lapiduzzz <Lapiduz@ms>
2022-07-07 21:33:59 +0300

    add adaptive

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,3 +15,18 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
     }
 }
 
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+    if (stage === "build-html") {
+        actions.setWebpackConfig({
+            module: {
+                rules: [
+                    {
+                        test: /offending-module/,
+                        use: loaders.null(),
+                    },
+                ],
+            },
+        })
+    }
+}
+
